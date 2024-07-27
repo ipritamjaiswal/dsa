3bc3a6823e11806045086e031b986b88a44ce8ac
pritamjswl <hello.pritamjaiswal@gmail.com>
2024-07-27 02:13:33 +0000

    Circular linked list

--- 1_Lists/circular_linked_list/cl_list.c
+++ 1_Lists/circular_linked_list/cl_list.c
@@ -53,5 +53,94 @@ Node* create_node(int data) {
 void insert_node(Node **head, int data) {
     Node *new_node = create_node(data);
 
-    
+    if (*head == NULL) {
+        *head = new_node;
+        (*head)->next = *head;
+        return;
+    }
+
+    // Traverse to the last element
+    Node *temp = *head;
+    while (temp->next != *head) {
+        temp = temp->next;
+    }
+
+    // Insert the new node at the end
+    temp->next = new_node;
+    new_node->next = *head;
+}
+
+void display_node(Node *head) {
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    Node *temp = head;
+    do {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+    printf("\n");
+}
+
+void delete_node(Node **head, int key) {
+    if (*head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    Node *current = *head, *prev = NULL;
+
+    // Check if the head node contains the key
+    if ((*head)->data == key) {
+        // Traverse to end of list
+        while (current->next != *head) {
+            current = current->next;
+        }
+
+        // Update the head pointer
+        if (current == *head) { // Only one node in the list
+            free(*head);
+            *head = NULL;
+        } else {
+            current->next = (*head)->next;
+            free(*head);
+            *head = current->next;
+        }
+        return;
+    }
+
+    // Traverse to the node before the key
+    prev = *head;
+    current = (*head)->next;
+    while (current != *head && current->data != key) {
+        prev = current;
+        current = current->next;
+    }
+
+    // If the key is not found
+    if (current == *head) {
+        printf("Key not found\n");
+        return;
+    }
+
+    // Delete the node
+    prev->next = current->next;
+    free(current);
+}
+
+
+void free_node(Node *head) {
+    if (head == NULL) {
+        return;
+    }
+
+    Node *current = head;
+    while (current->next != head) {
+        Node *temp = current;
+        current = current->next;
+        free(temp);
+    }
+    free(current);
 }
