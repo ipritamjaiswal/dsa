08f64646276283d0f393d42ae08c08ebcf42d6ce
Pritam Jaiswal <pritamjaiswal.me@gmail.com>
2024-09-17 08:34:51 +0000

    Did diamond pattern

--- 1_Patterns/main.cpp
+++ 1_Patterns/main.cpp
@@ -64,30 +64,53 @@ void print_pyramid(int n) {
 }
 
 void print_hollow_diamond(int n, char c) {
+    int s = 1;
+    // Print upper half
     for (int i = 0; i < n; i++) {
-        // Print space 
-        for (int j = n - i; j > 0; j--) {
+        // Print space before the character
+        for (int j = n - i - 1; j > 0; j--) {
             cout << "  ";
         }
+        // Print the character
         cout << c << " ";
-        // Print space
-        for (int j = 0; j < i; j++) {
-            for (int k = 0; k <= j; k++) {
+        // Print space after the character
+        if (i > 0) {
+            for (int j = 0; j < s; j++) {
                 cout << "  ";
             }
+            s+=2;
         }
-        
+        // Print the character from 2nd line
         (i > 0) ? cout << c << endl : cout << endl;
     }
+
+    s = s - 4;
+    // Print lower half
+    for (int i = 0; i < n - 1; i++) {
+        // Print space before the character
+        for (int j = 0; j < i + 1; j++) {
+            cout << "  ";
+        }
+        // Print the character
+        cout << c << " ";
+        // Print space after the character
+        for (int j = 0; j < s; j++) {
+            cout << "  ";
+        }
+        s -= 2;
+        // Print the character
+        (i < n - 2) ? cout << c << endl : cout << endl;
+    }
+    
 }
 
 int main(void) {
-    int n = 5;
-    // print_triangle_half(n);
-    // print_triangle_half(n, '*');
-    // print_floyd_triangle(n);
-    // print_inverted_triangle(n);
-    // print_pyramid(n);
+    int n = 10;
+    print_triangle_half(n);
+    print_triangle_half(n, '*');
+    print_floyd_triangle(n);
+    print_inverted_triangle(n);
+    print_pyramid(n);
     print_hollow_diamond(n, '*');
     return 0;
 }
